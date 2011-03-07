5d0b817f9d6871b4ef3818ae77e5a8ab128cdfe0
mobilemelting <jp@mobile-melting.de>
2011-03-07 11:11:30 +0100

    added 'parse_as' class method to parse a single or an array of Ratings in xml or json format

--- lib/rating.rb
+++ lib/rating.rb
@@ -3,4 +3,34 @@ class Rating < ActiveRecord::Base
   belongs_to :rateable, :polymorphic => true
   
   validates_uniqueness_of :user_id, :scope => [:rateable_id, :rateable_type]
+
+  class << self
+
+    ##
+    # Parse the specified array of [TourProduct]s in the requested format.
+    #
+    # === Parameters
+    #
+    # [rating] the array of Rating records
+    # [output] the requested output format, must be :xml or :json
+    #
+    # @return the "ratings" in the requested structure, e.g. xml format string
+    #
+    def parse_as(ratings, output = :xml)
+      if output == :xml
+        ratings.to_xml(:only => [:user_id, :free_text], :methods => [:rate_score])
+      elsif output == :json
+        ratings.to_json(:only => [:user_id, :free_text], :methods => [:rate_score])
+      end
+    end
+
+  end # end class methods
+
+  ##
+  # Returns this rating's rate score, e.g. 6
+  #
+  def rate_score
+    rate.score
+  end
+
 end
